ee568db0aaf4defa3e8ee0665ae738a807845d7e
Eric LING <eriling@e2r5p11.clusters.42paris.fr>
2022-02-18 10:10:34 +0100

    save

--- include/iterator_vector.hpp
+++ include/iterator_vector.hpp
@@ -6,14 +6,13 @@
 namespace ft{
 
 template <class T>
-class iterator_vector : public ft::iterator_traits <T>
-{
+class iterator_vector : ft::random_access_iterator_tag{
 
 public:
-		typedef typename ft::iterator_traits <T>::value_type value_type;
-		typedef typename ft::iterator_traits <T>::pointer pointer;
-		typedef typename ft::iterator_traits <T>::reference reference;
-		typedef typename ft::iterator_traits <T>::iterator_category iterator_category;
+		typedef T value_type;
+		typedef T* pointer;
+		typedef T& reference;
+		typedef ft::random_access_iterator_tag iterator_category;
 
     typedef iterator_vector<T> _it;
     iterator_vector(pointer ptr):_ptr(ptr){};
@@ -21,7 +20,7 @@ public:
     iterator_vector(const _it &ref);
 
 
-		iterator_vector &operator=(iterator_vector const &p) { _ptr = p.operator->(); return (*this); };
+	iterator_vector &operator=(iterator_vector const &p) { _ptr = p.operator->(); return (*this); };
     bool operator ==(iterator_vector const& p) const { return (_ptr == p._ptr); };
     bool operator !=(iterator_vector const& p) const { return (_ptr != p._ptr); };
     iterator_vector operator ++(int) { _ptr++; return (iterator_vector(_ptr - 1)); };
