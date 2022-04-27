844645bd5f9100127af2045121edd0bb450fc02c
Eric LING <eriling@e3r10p23.clusters.42paris.fr>
2022-04-27 16:25:17 +0200

    fixing test: more.cpp

--- src/my_main.cpp
+++ src/my_main.cpp
@@ -26,67 +26,12 @@ struct Buffer
 #define COUNT (MAX_RAM / (int)sizeof(Buffer))
 
 
-template <typename T>
-class foo {
-	public:
-		typedef T	value_type;
-
-		foo(void) : value(), _verbose(false) { };
-		foo(value_type src, const bool verbose = false) : value(src), _verbose(verbose) { };
-		foo(foo const &src, const bool verbose = false) : value(src.value), _verbose(verbose) { };
-		~foo(void) { if (this->_verbose) std::cout << "~foo::foo()" << std::endl; };
-		void m(void) { std::cout << "foo::m called [" << this->value << "]" << std::endl; };
-		void m(void) const { std::cout << "foo::m const called [" << this->value << "]" << std::endl; };
-		foo &operator=(value_type src) { this->value = src; return *this; };
-		foo &operator=(foo const &src) {
-			if (this->_verbose || src._verbose)
-				std::cout << "foo::operator=(foo) CALLED" << std::endl;
-			this->value = src.value;
-			return *this;
-		};
-		value_type	getValue(void) const { return this->value; };
-		void		switchVerbose(void) { this->_verbose = !(this->_verbose); };
-
-		operator value_type(void) const {
-			return value_type(this->value);
-		}
-	private:
-		value_type	value;
-		bool		_verbose;
-};
-
-template <typename T>
-std::ostream	&operator<<(std::ostream &o, foo<T> const &bar) {
-	o << bar.getValue();
-	return o;
-}
-// --- End of class foo
-
-template <typename T>
-T	inc(T it, int n)
-{
-	while (n-- > 0)
-		++it;
-	return (it);
-}
-
-template <typename T>
-T	dec(T it, int n)
-{
-	while (n-- > 0)
-		--it;
-	return (it);
-}
-
 #define _pair ft::pair
 #define TESTED_NAMESPACE ft
 #define T1 int
-#define T2 char
-typedef _pair<const T1, T2> T3;
-
+#define T2 std::string
 
 static int iter = 0;
-
 
 struct ft_more {
 	bool	operator()(const T1 &first, const T1 &second) const {
@@ -94,6 +39,7 @@ struct ft_more {
 	}
 };
 
+typedef _pair<const T1, T2> T3;
 typedef TESTED_NAMESPACE::map<T1, T2, ft_more> ft_mp;
 typedef TESTED_NAMESPACE::map<T1, T2, ft_more>::iterator ft_mp_it;
 
@@ -125,52 +71,15 @@ void	printSize(T_MAP const &mp, bool print_content = 1)
 #include <list>
 int		main(void)
 {
-	// ft::map<int, std::string> mp;
+	ft_mp mp;
 
-	// mp[42] = "fgzgxfn";
-	// mp[25] = "funny";
-	// mp[80] = "hey";
-	// mp[12] = "no";
-	// mp[27] = "bee";
-	// mp[90] = "8";
-	// printSize(mp);
-
-
-	std::list<T3> lst;
-
-	unsigned int lst_size = 7;
-	for (unsigned int i = 0; i < lst_size; ++i)
-		lst.push_back(T3('a' + i, lst_size - i));
-	TESTED_NAMESPACE::map<T1, T2> foo(lst.begin(), lst.end());
-
-	lst.clear(); lst_size = 4;
-	for (unsigned int i = 0; i < lst_size; ++i)
-		lst.push_back(T3('z' - i, i * 5));
-	TESTED_NAMESPACE::map<T1, T2> bar(lst.begin(), lst.end());
-
-	TESTED_NAMESPACE::map<T1, T2>::const_iterator it_foo = foo.begin();
-	TESTED_NAMESPACE::map<T1, T2>::const_iterator it_bar = bar.begin();
-
-	std::cout << "BEFORE SWAP" << std::endl;
-
-	std::cout << "foo contains:" << std::endl;
-	printSize(foo);
-	std::cout << "bar contains:" << std::endl;
-	printSize(bar);
-
-	foo.swap(bar);
-
-	std::cout << "AFTER SWAP" << std::endl;
-
-	std::cout << "foo contains:" << std::endl;
-	printSize(foo);
-	std::cout << "bar contains:" << std::endl;
-	printSize(bar);
-
-	std::cout << "Iterator validity:" << std::endl;
-	std::cout << (it_foo == bar.begin()) << std::endl;
-	std::cout << (it_bar == foo.begin()) << std::endl;
-
+	mp[42] = "fgzgxfn";
+	mp[25] = "funny";
+	mp[80] = "hey";
+	mp[12] = "no";
+	mp[27] = "bee";
+	mp[90] = "8";
+	printSize(mp);
 
 	return (0);
 }
